d46cf1baec211461858b7bee86b1464faa470bad
Hotdog Water <154757374+HotdogW@users.noreply.github.com>
2026-02-04 12:47:00 -0500

    Add files via upload
    added the poem and added a few new animations, now its just the second page

--- vFile.js
+++ vFile.js
@@ -16,19 +16,23 @@ setTimeout(()=>{
     document.getElementById("line7").classList.add("second")
 }, 27000 );
 setTimeout(()=>{
+    document.getElementById("line3").style.fontSize = "2em"
     document.getElementById("line4").style.fontSize = "2em"
     document.getElementById("line4").style.marginBottom = "0em"
     document.getElementById("line5").style.fontSize = "0.83em"
 
-    document.getElementById("line4").innerHTML = "Happy Valentine's Day"
+    document.getElementById("line3").innerHTML = "I'll love you forever."
+    document.getElementById("line4").innerHTML = "Happy Valentine's Day!"
     document.getElementById("line5").innerHTML = "Click Me!"
     document.getElementById("test").innerHTML = "💜"
 
+    document.getElementById("line3").classList.remove("second")
     document.getElementById("line4").classList.remove("second")
     document.getElementById("line5").classList.remove("second")
 
-    document.getElementById("line4").classList.add("line1")
-    document.getElementById("line5").classList.add("line3")
+    document.getElementById("line3").classList.add("line1")
+    document.getElementById("line4").classList.add("line2")
+    document.getElementById("line5").classList.add("line4")
     document.getElementById("test").classList.add("test")
     document.getElementById("test").classList.add("heart1")
 
@@ -41,7 +45,7 @@ setTimeout(()=>{
 setTimeout(()=>{
     document.getElementById("test").classList.remove("heart1")
     document.getElementById("test").classList.add("heart2")
-}, 32000 );
+}, 35000 );
 function growDiv(){
     document.getElementById("test").classList.remove("heart2")
     test.style.transform = "scale(500)"
